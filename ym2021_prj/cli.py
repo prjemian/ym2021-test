f20945c8c3a9bf2da500ad5d28aeeaddaeb447e0
Pete R Jemian <prjemian@gmail.com>
2021-11-22 14:24:20 -0600

    CI #3 disable

--- ym2021_prj/cli.py
+++ ym2021_prj/cli.py
@@ -16,5 +16,5 @@ def cli_args():
 
 def main():
     # simple function of this package, return a string
-    args = cli_args()  # FIXME: fails in CI
+    # args = cli_args()  # FIXME: fails in CI
     return f"main {__version__ = } of {__file__ = }"
