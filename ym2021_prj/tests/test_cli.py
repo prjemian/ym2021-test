ae5b87986e3ed913bf126b9cfe55ae588f81670e
Pete R Jemian <prjemian@gmail.com>
2021-11-23 11:12:39 -0600

    TST

--- ym2021_prj/tests/test_cli.py
+++ ym2021_prj/tests/test_cli.py
@@ -4,4 +4,4 @@ from .. import cli
 def test_cli():
     response = cli.main()
     assert isinstance(response, str)
-    assert response.startswith("main")
+    assert response.find("ym2021_prj") >= 0
