cb6bde6dcf8f91569ac793cd98fbeb8aa0c2f320
Pete R Jemian <prjemian@gmail.com>
2021-12-28 11:26:55 -0600

    test for env var

--- ym2021_prj/tests/test_cli.py
+++ ym2021_prj/tests/test_cli.py
@@ -1,3 +1,6 @@
+import os
+import pytest
+
 from .. import cli
 
 
@@ -5,3 +8,15 @@ def test_cli():
     response = cli.main()
     assert isinstance(response, str)
     assert response.find("ym2021_prj") >= 0
+
+@pytest.mark.parametrize(
+    "var_name, exists, value",
+    [
+        ["TESTABLE", True, "testable"],
+        ["NO_SUCH_ENV_VAR", False, None],
+    ]
+)
+def test_env_var(var_name, exists, value):
+    assert (var_name in os.environ) is exists
+    env_var = os.environ.get(var_name)
+    assert env_var == value
