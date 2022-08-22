a81123d081c8e39732fdb6d972b0b85a62e13985
Pete R Jemian <prjemian@gmail.com>
2022-08-22 14:35:08 -0500

    MNT #3

--- ym2021_prj/cli.py
+++ ym2021_prj/cli.py
@@ -2,24 +2,26 @@
 Python package template with command-line interface
 """
 
-import argparse
 from . import __version__
+import argparse
 
 
 def cli_args():
     """Define & examine command-line arguments & options."""
-    # print(f"{__package__=}")
-    # _desc = f"{__package__}: " + __doc__.strip().splitlines()[0]
-    _desc = __doc__.strip().splitlines()[0]
-    p = argparse.ArgumentParser(description=_desc)
+    p = argparse.ArgumentParser(
+        prog=__package__,
+        description=__doc__.strip().splitlines()[0],
+        epilog=f"{__package__} is for demonstration.",
+    )
     p.add_argument("-v", "--version", action="version", version=__version__)
     return p.parse_args()
 
 
 def main():
+    args = cli_args()
     # simple function of this package, return a string
-    args = cli_args()  # FIXME: fails in CI
-    return f"{__name__} version {__version__} of '{__file__}', args={args}"
+    s = f"{__name__} version {__version__} of '{__file__}', args={args}"
+    return s
 
 
 if __name__ == "__main__":
