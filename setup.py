60cd3a4502a7316477c88249c6252d7d2d71ff40
Pete R Jemian <prjemian@gmail.com>
2022-08-23 12:30:04 -0500

    CI #10 try it

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 import setuptools
-from setuptools_scm import get_version
+import setuptools_scm
 
-# adapters for use in conda's meta.yaml file
-version = get_version()
-del get_version
 
-setuptools.setup()
+def get_my_version():
+    import os
+
+    # https://docs.conda.io/projects/conda-build/en/latest/user-guide/environment-variables.html#git-environment-variables
+    version = os.environ.get("GIT_DESCRIBE_TAG")
+    if version is not None:
+        build_string = os.environ.get("GIT_BUILD_STR")
+        if len((build_string or "").strip()) > 0:
+            version += f"+{build_string}"
+    else:
+        version = setuptools_scm.get_version()
+    return version
+
+
+setuptools.setup(
+    version=get_my_version(),
+)
