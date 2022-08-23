009fe926c4ea242067e8c756702c70b509e280f7
Pete R Jemian <prjemian@gmail.com>
2022-08-23 04:55:20 -0500

    CI #10 wip - need version in meta.yaml

--- setup.py
+++ setup.py
@@ -1,19 +1,8 @@
-import configparser
 import setuptools
 from setuptools_scm import get_version
 
-setuptools.setup()
-
 # adapters for use in conda's meta.yaml file
-_setup_cfg = configparser.ConfigParser()
-_setup_cfg.read("setup.cfg")
-
-__pkg_description__ = _setup_cfg.get("metadata", "description")
-__pkg_keywords__ = _setup_cfg.get("metadata", "keywords")
-__pkg_license__ = _setup_cfg.get("metadata", "license")
-__pkg_name__ = _setup_cfg.get("metadata", "name")
-__pkg_url__ = _setup_cfg.get("metadata", "url")
-__pkg_version__ = get_version()
-
-del _setup_cfg
+version = get_version()
 del get_version
+
+setuptools.setup()
