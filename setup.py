d36fda6a6de6bfa7e63f58d848d752a2c4842273
Pete R Jemian <prjemian@gmail.com>
2022-08-22 19:24:55 -0500

    PKG #10 wip

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ from setuptools_scm import get_version
 setuptools.setup()
 
 # adapter for use in conda's meta.yaml file
-conda_meta = dict(version=get_version())
 _setup_cfg = configparser.ConfigParser()
 _setup_cfg.read("setup.cfg")
+conda_meta = dict(version=get_version())
 for _key in "description keywords license name url".split():
     conda_meta[_key] = _setup_cfg.get("metadata", _key)
+
+elbow = "wrist"
 
 del _key
 del _setup_cfg
