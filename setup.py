081e4c72430f04cffe2342a586889f89ac27bef2
Pete R Jemian <prjemian@gmail.com>
2022-08-23 13:14:10 -0500

    CI #10 version from git describe --tags

--- setup.py
+++ setup.py
@@ -6,12 +6,8 @@ def get_my_version():
     import os
 
     # https://docs.conda.io/projects/conda-build/en/latest/user-guide/environment-variables.html#git-environment-variables
-    version = os.environ.get("GIT_DESCRIBE_TAG")
-    if version is not None:
-        build_string = os.environ.get("GIT_BUILD_STR")
-        if len((build_string or "").strip()) > 0:
-            version += f"+{build_string}"
-    else:
+    version = os.environ.get("git_describe")
+    if version is None:
         version = setuptools_scm.get_version()
     return version
 
