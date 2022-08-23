b666f275c59bab3bc1ee9e14cfcfe6f656a2be1e
Pete R Jemian <prjemian@gmail.com>
2022-08-23 16:29:59 -0500

    CI #10 wip

--- setup.py
+++ setup.py
@@ -2,23 +2,8 @@ import setuptools
 import setuptools_scm
 
 
-def get_my_version():
-    import os
-
-    # https://docs.conda.io/projects/conda-build/en/latest/user-guide/environment-variables.html#git-environment-variables
-    version = os.environ.get("version_from_scm")
-    if version is None:
-        version = setuptools_scm.get_version()
-    else:
-        parts = version.split("-")
-        version = parts[0]
-        if len(parts) > 1:
-            version += f".dev{parts[1]}"
-        if len(parts) > 2:
-            version += f"+{parts[2].split('.')[0]}"
-    return version
-
-
 setuptools.setup(
-    version=get_my_version(),
+    version=setuptools_scm.get_version(),
+    # use_scm_version=True,
+    setup_requires=['setuptools_scm'],
 )
