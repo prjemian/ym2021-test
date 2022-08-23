1bb39b2db87f6fc29d8ddf7e0ac2e9cdfc16890c
Pete R Jemian <prjemian@gmail.com>
2022-08-22 19:19:09 -0500

    PKG #10 apply adaptet

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
+import configparser
 import setuptools
 from setuptools_scm import get_version
 
 setuptools.setup()
-__pkg_version__ = get_version()
+
+# adapter for use in conda's meta.yaml file
+conda_meta = dict(version=get_version())
+_setup_cfg = configparser.ConfigParser()
+_setup_cfg.read("setup.cfg")
+for _key in "description keywords license name url".split():
+    conda_meta[_key] = _setup_cfg.get("metadata", _key)
+
+del _key
+del _setup_cfg
