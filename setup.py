1b2a725409ca028dac69bdee184016957829ae7c
Pete R Jemian <prjemian@gmail.com>
2022-08-23 13:53:11 -0500

    CI #10 try again

--- setup.py
+++ setup.py
@@ -6,9 +6,16 @@ def get_my_version():
     import os
 
     # https://docs.conda.io/projects/conda-build/en/latest/user-guide/environment-variables.html#git-environment-variables
-    version = os.environ.get("git_describe")
+    version = os.environ.get("GIT_DESCRIBE")
     if version is None:
         version = setuptools_scm.get_version()
+    else:
+        parts = version.split("-")
+        version = parts[0]
+        if len(parts) > 1:
+            version += f".dev{parts[1]}"
+        if len(parts) > 2:
+            version += f"+{parts[2].split('.')[0]}"
     return version
 
 
