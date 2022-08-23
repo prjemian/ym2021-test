5575ce55168582692fd3c022a49ca4012e806cb5
Pete R Jemian <prjemian@gmail.com>
2022-08-23 16:36:13 -0500

    STY #10

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ import setuptools_scm
 setuptools.setup(
     version=setuptools_scm.get_version(),
     # use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    setup_requires=["setuptools_scm"],
 )
